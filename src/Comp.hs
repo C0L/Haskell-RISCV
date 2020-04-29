fb087c7f6f03914b73df79731e1009383913a883
C0L <colindrewes@gmail.com>
2020-04-29 00:18:16 -0700

    Redoing stack manipulation

--- src/Comp.hs
+++ src/Comp.hs
@@ -5,47 +5,55 @@ import ASTtypes
 import Text.Printf
 
 type Bytes = Int 
+type String = ID
 
--- scope list 
-
-compMain :: Expression -> Bytes -> String
-compMain exp sp = case exp of 
-  (Prog exp)                        -> defHeader ++ (compMain sp exp)
-  (Main exp)                        -> "main:\n" ++ prologue ++ (compMain (sp + 16) exp)
-  (IfExp (BinOp bop b0 b1) e1)      -> case bop of 
+compMain :: Expression -> [(Bytes, ID)] -> String
+compMain exp vars = case exp of 
+  (Prog e0)                         -> defHeader ++ (compMain e0 vars)
+  (Main e0)                         -> "main:\n" ++ prologue ++ (compMain exp vars)
+  (IfExp (BinOp bop b0 b1) e0)      -> case bop of 
                                           Le  -> "\tblt\n"    -- Branch less than
                                           Lt  -> "\tble\n"    -- Branch less than or equal
                                           Ne  -> "\tbne\n"    -- Branch not equal
                                           Eq  -> "\tbeq\n"    -- Branch equal
-  (DeclareInt id e0)                -> 
-  (EvalInt exp)                     -> show exp
-  (BinOp bop e0 e1)                 -> (compBop bop (compMain e0 sp) (compMain e1 sp))
-  (RetV exp)                        -> epilogue ++ (return (compMain exp sp) sp)
+  (DeclareInt id e0)                -> (allocInt e0) ++ (compMain e0 (addSSInt vars id))
+  (EvalInt e0)                      -> show exp
+  (BinOp bop e0 e1)                 -> (compBop bop (compMain e0 vars) (compMain e1 vars))
+  (RetV e0)                         -> epilogue ++ (return (compMain exp vars))
+
+
+-- Save the corresponding address of the int with its name for further lookup
+addSSInt :: [(Bytes, ID)] -> ID -> [(Bytes, ID)] 
+addSSInt vars id = vars:((fst (last vars)), id)
+
+-- Make space on the stack for the new integer
+allocInt :: Int -> String
+allocInt val = "\taddi\tsp,sp,-4\n \
+                \ "
 
 
 
-allocInt :: String -> Int -> String
-allocInt id val = "\tli\ta0,%s\n"
-
---dellocInt :: String 
+dellocInt :: [(Bytes, ID)] -> String
 
 return :: Int -> String
 return rv = printf "\tli\ta0,%s\n \
                    \ \tret\n" rv
 
-
 defHeader :: String
 defHeader = "\t.text\n \
             \ \t.align\t1\n \
             \ \t.global\t.main\n"
- 
+
+
 prologue :: String  -- Make space on stack and save reg state
 prologue = "\taddi\tsp,sp,16\n \
            \ \tad\tra, 0(sp)\n"
 
+
 epilogue :: String  -- Restore registers and stack pointer
 epilogue sp = printf "\tld\tra,0(sp)\n \     
                      \ \taddi\tsp,sp,-16\n"
+
 
 compBop :: BinaryOperator -> String -> String -> String
 compBop exp b0 b1 = case exp of 
