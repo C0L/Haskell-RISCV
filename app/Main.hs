05c6b052d38c1c3f9163ef123188b1545f618c00
Colin Drewes <colindrewes@gmail.com>
2020-05-04 22:43:36 -0700

    Simple cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,14 @@
+{- |
+Module      :  Main.hs
+Description :  Tokenizes, parses, and compiles stdin
+Copyright   :  
+License     :  
+
+Maintainer  :  colindrewes@gmail.com
+Stability   :  
+Portability : 
+-}
+
 module Main where
 
 import ASTtypes
@@ -5,7 +16,7 @@ import Parser
 import Lexer
 import Comp
 
-
+-- | Pipe stdin into the compilation process 
 main :: IO ()
 main = do
   input <- getContents
--- src/Comp.hs
+++ src/Comp.hs
@@ -1,3 +1,14 @@
+{- |
+Module      :  Comp.hs
+Description :  Converts AST tree into a asm
+Copyright   :  
+License     :  
+
+Maintainer  :  colindrewes@gmail.com
+Stability   :  
+Portability : 
+-}
+
 module Comp where
 
 import Parser
@@ -6,6 +17,7 @@ import ASTtypes
 import Text.Printf
 import Text.Read
 
+-- | Internal types for conversion
 type Bytes = Int 
 type Offset = Int
 type ID = String
@@ -16,17 +28,20 @@ type Scope = [(Offset,ID)]
 type ProgData = (Scope, ASM, Mark)
 
 
---Check if correct stack space value is found
+{- | Starts the compilation process with starting values -}
 compMain :: Expression -> [Token] -> ASM
 compMain exp toks = (defHeader sSpace) ++ res
   where 
     sSpace       = stackSpace toks 0
     (scope, res, mk) = (compExp exp "x5" [] 0)
 
--- Store associated register in the scope var??
--- Reset these values after a if or func
--- Unsafe IO to generate random string???
--- NEED AN ADD SCOPE FUNCTIOION THAT CAN ALSO CALL ON LINE BREAKS
+
+{- 
+   | Converts expression into the return assembly value
+   | Reg is the current register to stores results in
+   | Scope holds the variable names and address which are in scope
+   | Mark is the current jump marker value in asm
+-}
 compExp :: Expression -> Reg -> Scope -> Mark -> ProgData
 compExp exp reg scope mark = case exp of 
   (Prog e0)                         -> compExp e0 reg scope mark 
@@ -69,7 +84,13 @@ compExp exp reg scope mark = case exp of
                                        in (s0, asm ++ epilogue ++ funcRet, mk0)
 
 
--- If statement with mutliple if clauses
+{- 
+   | Evaluate chained else statements
+   | [Expression] is the chain of else statement expressions
+   | Reg is the register to dump a final result in 
+   | Scope contains the variables and their address
+   | Mark is for unique jump markers 
+-}
 multiConditionalBlock :: [Expression] -> Reg -> Scope -> Mark -> ASM
 multiConditionalBlock []        reg scope mk = noOP
 multiConditionalBlock (e:elses) reg scope mk = case e of 
@@ -81,7 +102,14 @@ multiConditionalBlock (e:elses) reg scope mk = case e of
                                       in asm1 ++ asm2 ++ (conditionalBlock bop asm3 (multiConditionalBlock elses reg scope (mk+2)) mk) -- Infinite chainability
 
 
--- Assume that the predicates are in register x5 and x6
+
+{- 
+   | Evaluate indivudial if statements
+   | BinaryOperator is the type of assembly instruction to branch on
+   | ASM The positive branch instructions
+   | ASM The negative branch instructions
+   | Mark is for unique jump markers 
+-}
 conditionalBlock :: BinaryOperator -> ASM -> ASM -> Mark -> ASM
 conditionalBlock bop posExp negExp mk = printf "\t%s\tx5,x6,mark_%d:\n\
                                                \%s\                        
@@ -97,19 +125,22 @@ conditionalBlock bop posExp negExp mk = printf "\t%s\tx5,x6,mark_%d:\n\
       Ne -> "bne"
 -- ADD GREATER THAN OPTIONS 
 
-
+{-| Set true to the given register -}
 trueASM :: Reg -> ASM
 trueASM reg = printf "\tli\t%s,1\n" reg
 
 
+{-| Set false to the given register -}
 falseASM :: Reg -> ASM
 falseASM reg = printf "\tli\t%s,0\n" reg
 
 
+{-| Does nothing, used as a filler for non evaluated branches-}
 noOP :: ASM
 noOP = "\taddi\tx0,x0,0\n"
 
--- Start the first value above the fp and ra, all later storage locations are based off this
+
+{- | Start the first value above the fp and ra, all later storage locations are based off this -}
 addInteger :: Scope -> ID -> Int -> Mark -> ProgData
 addInteger [] id val mk = ([(16, id)], (storeLitStack val 16), mk)
 addInteger sc id val mk = ([(nAddress, id)] ++ sc, storeLitStack val nAddress, mk)
@@ -117,12 +148,13 @@ addInteger sc id val mk = ([(nAddress, id)] ++ sc, storeLitStack val nAddress, m
     nAddress = (fst (head sc)) + 8
 
 
--- Potentially add types and pass that in here
+{- | Store a literal on the stack -}
 storeLitStack :: Int -> Bytes -> ASM
 storeLitStack val add = printf "\tli\tx5,0x%x\n\
                             \\tsw\tx5,%d(sp)\n" val add
 
 
+{- | Store a register on the stack -}
 storeRegStack :: Reg -> ID -> Scope -> ASM
 storeRegStack reg id []     = error "Major issue, var not in scope"
 storeRegStack reg id (x:xs) = if id == (snd x)
@@ -130,6 +162,7 @@ storeRegStack reg id (x:xs) = if id == (snd x)
                               else storeRegStack reg id xs
 
 
+{- | Retrieve a value from the stack -}
 retrieveStack :: Reg -> ID -> Scope -> ASM
 retrieveStack reg id []     = error "Major issue, var not in scope" 
 retrieveStack reg id (x:xs) = if id == (snd x) 
@@ -137,14 +170,17 @@ retrieveStack reg id (x:xs) = if id == (snd x)
                               else retrieveStack reg id xs
 
 
+{- | Load a literal value into an address-}
 loadLiteral :: Reg -> Int -> ASM
 loadLiteral reg val = printf "\tli\t%s,%d\n" reg val
 
 
+{- | General purpose return instruction -}
 funcRet :: ASM
 funcRet = "\tjr\tra\n" 
 
 
+{- | General purpose header for all our asm files -}
 defHeader :: Bytes -> ASM
 defHeader stackSize =  printf  "\t.equ\tFP_OFFSET,-16\n\
                                 \\t.equ\tLOCAL_VARS,-%d\n\
@@ -152,28 +188,23 @@ defHeader stackSize =  printf  "\t.equ\tFP_OFFSET,-16\n\
                                 \\t.align\t1\n\
                                 \\t.globl\tmain\n" stackSize
 
--- fp should be s0
--- 8 bytes for the FP byte integer, 8 per other int 
--- Add x many bytes for the local vars where 8 bytes is 1 int
--- Add 16 bytes more for the fp and the ra of the caller
--- Store the frame pointer at 0 - (16+x)
--- Store the rerturn address at 8 - (16+x)
--- I beleive this is where the var storage starts
--- Add 16 + x bytes to the stack point and make this the new frame pointer
+{- | Prologue for all function calls, save the fp and the ra for return -}
 prologue :: ASM
 prologue =  "\taddi\tsp,sp,LOCAL_VARS\n\
              \\taddi\tsp,sp,FP_OFFSET\n\
              \\tsw\tfp,0(sp)\n\
              \\tsw\tra,8(sp)\n"
 
-epilogue :: ASM    -- Restore registers and stack pointer
+
+{- | Epilogue for all function calls to restore the fp and the ra -}
+epilogue :: ASM    
 epilogue = printf  "\tlw\tfp,8(sp)\n\
                     \\tlw\tra,16(sp)\n\
                     \\taddi\tsp,sp,-LOCAL_VARS\n\
                     \\taddi\tsp,sp,-FP_OFFSET\n"
 
 
-
+{- | Precalculate the stack space that will be needed for the program as a whole -}
 stackSpace :: [Token] -> Bytes -> Bytes
 stackSpace [] stackSize     = stackSize
 stackSpace (x:xs) stackSize = case x of 
